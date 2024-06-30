0c1a10b5bdd992b87bee432a0e1a87c15891571e
Ryosuke Asano <s1F102400011@iniad.org>
2024-06-30 22:38:19 +0900

    part2

--- app.py
+++ app.py
@@ -15,6 +15,23 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count, takenoko_count, messeages
+    if request.form.get("item") == "kinoko":
+        kinoko_count += 1
+    elif request.form.get("item") == "takenoko":
+        takenoko_count += 1
+
+    messeages.append(request.form.get("message"))
+
+    if (len(messeages) > 3):
+        messeages.pop(0)
+
+    html_messeages = ""
+    for i in range(len(messeages)):
+        messeage = messeages[i]
+        messeage_html = f'<div class="alert alert-warning ms-5" role="alert"> {messeage} </div>'
+        html_messeages += messeage_html
+
     kinoko_percent = (kinoko_count / (kinoko_count + takenoko_count)) * 100
     takenoko_percent = (takenoko_count / (kinoko_count + takenoko_count)) * 100
     return render_template('vote.html', **vars())
